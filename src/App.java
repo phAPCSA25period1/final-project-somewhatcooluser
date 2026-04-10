1159eb84915d5ac2ad17728a2d3077b2130c0a55
somewhatcooluser <aidenjt@gmail.com>
2026-04-10 16:41:17 +0000

    Create a date and test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int year = 1990;
+        int month = 1;
+        int day = 1;
+        String currentDate = year + "-" + month + "-" + day;
+        System.out.println("Please enter your birthdate.");
+        System.out.println();
+        System.out.println("Current date:")
+        System.out.println(currentDate);
     }
 }
